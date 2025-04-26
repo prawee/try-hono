47a748b5b6c5bfd6669dc7f2ac992077773a0221
Prawee Wongsa <prawee@hotmail.com>
2025-04-26 23:51:43 +0700

    try to deploy lambda func

--- lib/hono-stack.ts
+++ lib/hono-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from 'aws-cdk-lib'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apigw from 'aws-cdk-lib/aws-apigateway'
 
 export class HonoStack extends cdk.Stack {
     constructor(
@@ -8,6 +11,13 @@ export class HonoStack extends cdk.Stack {
         props?: cdk.StackProps
     ) {
         super(scope, id, props);
+
+        const fn = new NodejsFunction(this, 'lambda', {
+            depsLockFilePath: 'bun.lock',
+            entry: 'src/index.ts',
+            handler: 'handler',
+            runtime: lambda.Runtime.NODEJS_20_X
+        })
 
     }
 }
