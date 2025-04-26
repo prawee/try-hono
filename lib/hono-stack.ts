76c299a801c426d207ac268f6f85924517de47b5
Prawee Wongsa <prawee@hotmail.com>
2025-04-26 20:18:20 +0700

    create HonoStack lib

---
+++ lib/hono-stack.ts
@@ -0,0 +1,13 @@
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+
+export class HonoStack extends cdk.Stack {
+    constructor(
+        scope: Construct,
+        id: string,
+        props?: cdk.StackProps
+    ) {
+        super(scope, id, props);
+
+    }
+}
