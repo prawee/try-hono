812c473be86621481da2a61f175905d5b7f0d1f5
Prawee Wongsa <prawee@hotmail.com>
2025-05-03 19:25:36 +0700

    write script for seed data

---
+++ prisma/seed/index.ts
@@ -0,0 +1,20 @@
+import { users } from './user'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    console.log('prisma seed start...')
+    for (let user of users) {
+        await prisma.user.create({
+            data: user
+        })
+    }
+}
+
+main().catch(e => {
+    console.log(e)
+    process.exit(1)
+}).finally(() => {
+    prisma.$disconnect()
+})
