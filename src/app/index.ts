53678a47238d82a80fd040ae0d74f0722a2c99b1
Prawee Wongsa <prawee@hotmail.com>
2025-05-03 19:41:56 +0700

    update route of user

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { home } from './api/home'
+import { user } from './api/user'
 
 const app = new Hono()
 
 app.use("*", logger())
 
 app.route('/', home)
+app.route('/user', user)
 
 export default app
